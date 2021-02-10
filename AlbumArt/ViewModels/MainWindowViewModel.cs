4a080418c09cdf6d3677112e14bd628ffe5ca11a
Steven Kirk <grokys@gmail.com>
2021-02-10 15:23:22 +0100

    Display list of artists/albums according to a search term.

---
+++ AlbumArt/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Text;
+using iTunesSearch.Library;
+using iTunesSearch.Library.Models;
+using ReactiveUI;
+
+namespace AlbumArt.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly iTunesSearchManager _search;
+        private string?_searchText;
+        
+        public MainWindowViewModel()
+        {
+            _search = new iTunesSearchManager();
+            SearchResults = new ObservableCollection<Album>();
+            this.WhenAnyValue(x => x.SearchText)
+                .Throttle(TimeSpan.FromMilliseconds(300))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(DoSearch);
+        }
+
+        public string? SearchText
+        {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public ObservableCollection<Album> SearchResults { get; }
+
+        async void DoSearch(string? s)
+        {
+            SearchResults.Clear();
+
+            if (string.IsNullOrEmpty(s)) return;
+            
+            var result = await _search.GetAlbumsAsync(_searchText);
+
+            foreach (var album in result.Albums)
+            {
+                SearchResults.Add(album);                    
+            }
+        }
+    }
+}
